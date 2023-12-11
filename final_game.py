6f5f32ccdf3ea8d31877f2b1f5f1b6d5aa0b1bf3
MikelWinemi <124489963+MikelWinemi@users.noreply.github.com>
2023-12-10 21:35:14 -0500

    Add files via upload

---
+++ final_game.py
@@ -0,0 +1,53 @@
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("You're an adventurer who has spent his life searching for a long lost treasure that your grandpa used to tell you about before he passed away")
+print("Your story begins here!")
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You chose a door that leads to endless pit and you fall in. Game Over.")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    choice4 = input('You found a room that has a tiny statue of a dragon and you notice there is a lever but you do not trust it. You also notice a door leading to what might be another room. Type "pull" or "enter" \n').lower()
+    if choice4 == "enter":
+        print("A thousand spikes rush at you and you die. Game Over")
+    else:
+        choice5 = input('A secret door opens up and you enter and see the treasure box. Do you open it? Type "yes" or "no" \n').lower()
+        if choice5 == "yes":
+            print("Congratulations! You found the treasure! You win!")
+        else:
+            print("You decide to leave the treasure behind and continue your adventure. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
